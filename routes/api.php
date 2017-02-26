e0e499084f93da8c9bfc26fb779d670c0f3e5902
Viraj Khatavkar <viraj.2438@gmail.com>
2017-02-27 02:26:21 +0530

    Authentication for API routes

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::put('user/{user}/online', 'UserOnlineController');
-Route::put('user/{user}/offline', 'UserOfflineController');
+Route::middleware('auth:api')->put('user/{user}/online', 'UserOnlineController');
+Route::middleware('auth:api')->put('user/{user}/offline', 'UserOfflineController');
